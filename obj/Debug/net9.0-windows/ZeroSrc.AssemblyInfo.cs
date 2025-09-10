f171eb73ab1130465115b317455f6ddf2685f106
TZ <rozakadm@gmail.com>
2025-09-10 17:06:06 +0700

    Agend Update realse

--- obj/Debug/net9.0-windows/ZeroSrc.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/ZeroSrc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZeroSrc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf4605ece589e8e36d65b37ffb975088319ae369")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+843af6d72c94b078d247731c1f05dd1fe08a79ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZeroSrc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZeroSrc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
