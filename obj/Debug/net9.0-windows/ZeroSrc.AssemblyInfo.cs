94f5ced5c32a2d15a47f36fc47177f90a89e3fe8
TZ <rozakadm@gmail.com>
2025-10-12 17:03:11 +0700

    Update New Version

--- obj/Debug/net9.0-windows/ZeroSrc.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/ZeroSrc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZeroSrc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8507431868ad23bb79ab697dea46ee038ddc5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+659c1893a147558242f0c035b0ba410f75baf174")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZeroSrc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZeroSrc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
