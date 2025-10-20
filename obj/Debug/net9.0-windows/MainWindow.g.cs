61f3e8affde89a6e27dd0cc2d5bf2b444d497564
TZ <rozakadm@gmail.com>
2025-10-20 23:06:56 +0700

    Pre-Realse

--- obj/Debug/net9.0-windows/MainWindow.g.cs
+++ obj/Debug/net9.0-windows/MainWindow.g.cs
@@ -67,7 +67,7 @@ namespace ZeroMix {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ZeroMix;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ZeroMix;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- obj/Debug/net9.0-windows/SearchOverlay.g.cs
+++ obj/Debug/net9.0-windows/SearchOverlay.g.cs
@@ -84,7 +84,7 @@ namespace ZeroMix {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/ZeroMix;V1.0.0.0;component/searchoverlay.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ZeroMix;component/searchoverlay.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\SearchOverlay.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
