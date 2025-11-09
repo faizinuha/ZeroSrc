b86ff6e0d6f7807c9497b588a6ea961f82230193
TZ <rozakadm@gmail.com>
2025-11-09 20:37:08 +0700

    Update Fix

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,5 +7,17 @@ namespace ZeroMix
     public partial class App : Application
     {
         public static HotkeyCore? HotkeyCoreInstance { get; private set; }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Inisialisasi dan jalankan pengecekan update secara Asynchronous
+            var updater = new GithubUpdater();
+
+            // Panggil metode async. 
+            // Menggunakan 'async void' pada OnStartup aman karena ini adalah event handler.
+            await Task.Run(() => updater.CheckAndUpdateAsync());
+        }
     }
-}
+    }
