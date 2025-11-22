e7d8ef8c4ebb69860bc11dc9faa00d270d35aaa6
Mahiro-Ux <rtxalham@gmail.com>
2025-11-22 11:47:06 +0000

    >NET

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,7 +3,7 @@ using System.Windows;
 
 namespace ZeroMix
 {
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
         public static HotkeyCore? HotkeyCoreInstance { get; private set; }
 
