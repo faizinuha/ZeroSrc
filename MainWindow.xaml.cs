1216404421c04765792f0d53238232c58b8b179e
TZ <rozakadm@gmail.com>
2026-01-31 22:58:23 +0700

    Bump version to 2.5.0 in MainWindow.xaml.cs, ZeroMix.csproj, and build script

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -39,7 +39,7 @@ namespace ZeroMix
     
     public partial class MainWindow : Window
     {
-        private const string CURRENT_VERSION = "2.4.0";
+        private const string CURRENT_VERSION = "2.5.0";
         
         // Windows API for Taskbar transparency
         [DllImport("user32.dll", SetLastError = true)]
