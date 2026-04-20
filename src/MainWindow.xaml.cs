6babac0e3a8d459f88065483e1b9105221b60725
TZ <rozakadm@gmail.com>
2026-04-20 13:53:20 +0700

    chore: release v6.0.0

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -43,7 +43,7 @@ namespace ZeroMix
     
     public partial class MainWindow : Window, ZeroMix.Plugins.IZeroMixHost
     {
-        private const string CURRENT_VERSION = "6.0.1";
+        private const string CURRENT_VERSION = "6.0.0";
         
         // Windows API for Taskbar transparency
         [DllImport("user32.dll", SetLastError = true)]
